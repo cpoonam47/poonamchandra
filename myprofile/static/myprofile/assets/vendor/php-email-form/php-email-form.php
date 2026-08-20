7d51886f04fedc638189d2e30383c3dcbea50a7d
cpoonam47 <84739805+cpoonam47@users.noreply.github.com>
2026-08-20 14:27:57 +0530

    Add files via upload

---
+++ myprofile/static/myprofile/assets/vendor/php-email-form/php-email-form.php
@@ -0,0 +1,25 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $name = htmlspecialchars($_POST["name"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $subject = htmlspecialchars($_POST["subject"]);
+    $message = htmlspecialchars($_POST["message"]);
+
+    $to = "cpoonam47@gmail.com"; // Change to your email
+
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    $body = "Name: $name\n";
+    $body .= "Email: $email\n\n";
+    $body .= "Message:\n$message";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message.";
+    }
+}
+?>
